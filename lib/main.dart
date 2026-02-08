1928799a2a1ae2cfbd27c81e620c901b80d741bb
mexid <www.elmarbak7@hotmail.com>
2026-02-08 13:11:04 -0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:hive/hive.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
-
 import 'widgets/hud.dart';
 import 'game/dino_run.dart';
 import 'models/settings.dart';
